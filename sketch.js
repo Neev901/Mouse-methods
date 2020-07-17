93720aed2101369917f38ee4c24dcd168edafcb4
Neev901 <legend.neev@gmail.com>
2020-07-17 18:57:13 +0530

    Add new class of constrained body and add a new log and bird as bodyA and BodyB to the constrained class

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -35,6 +36,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    clog = new Log(230,180,80,PI);
+    chain = new slingshot(bird.body,clog.body)
+
 
 }
 
@@ -58,6 +62,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    clog.display();
+    chain.display();
 
     bird.display();
     platform.display();
