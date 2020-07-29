238398f6dd2d2e0c5d82fbb846a7e5a6c39cd0b9
Neev901 <legend.neev@gmail.com>
2020-07-29 18:41:29 +0530

    Used Mouse dragged and Mouse realsed functions to release the bird

--- slingshot.js
+++ slingshot.js
@@ -1,20 +1,25 @@
 class slingshot {
-constructor(bodyA, bodyB){
-var options={
-bodyA:bodyA,
-bodyB:bodyB,
-stiffness:0.03,
-length:10
-}
+  constructor(bodyA, pointB) {
+    var options = {
+      bodyA: bodyA,
+      pointB: pointB,
+      stiffness: 0.03,
+      length: 10
+    }
+    this.pointB=pointB
+    this.sling = Constraint.create(options)
+    World.add(world, this.sling)
 
-this.sling=Constraint.create(options)
-World.add(world,this.sling)
+  }
+  
+  display() {
+    if(this.sling.bodyA!==null){
+    var pointA = this.sling.bodyA.position;
+    line(pointA.x, pointA.y, this.sling.pointB.x, this.sling.pointB.y);
+  }
+  }
 
-}
-display(){
-  var pointA=this.sling.bodyA.position;
-  var pointB=this.sling.bodyB.position;
-line(pointA.x,pointA.y,pointB.x,pointB.y); 
-}
-
+  fly(){
+   this.sling.bodyA=null
+  }
 }
